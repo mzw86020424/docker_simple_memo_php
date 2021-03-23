89ff8cfc1b18c7229a579f9fa0953c8408d6b2d9
mzw86020424 <mzw86020424@gmail.com>
2021-03-23 10:15:32 +0900

    共通のヘッダー情報出力関数を作成

---
+++ common/header.php
@@ -0,0 +1,16 @@
+<?php
+// - タイトルを指定してヘッダーを作成する
+// - @param $title
+// - @return string
+
+function getHeader($title){
+  return <<<EOF 
+  <head>
+    <meta charset="UTF-8"
+    <title>SimpleMemo | {$title} </title>
+    <link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="../public/css/main.css">
+    <script defer src="../public/js/all.js" ></script>
+  </head>
+  EOF;
+}
