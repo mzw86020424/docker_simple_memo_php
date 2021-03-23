5676fb0d7e99b3788381752782dce7077a6524b4
mzw86020424 <mzw86020424@gmail.com>
2021-03-23 10:24:50 +0900

    ユーザー登録のビューを作成

--- common/header.php
+++ common/header.php
@@ -4,13 +4,13 @@
 // - @return string
 
 function getHeader($title){
-  return <<<EOF 
+  return <<<EOF
   <head>
-    <meta charset="UTF-8"
-    <title>SimpleMemo | {$title} </title>
-    <link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="../public/css/main.css">
-    <script defer src="../public/js/all.js" ></script>
+    <meta charset="utf-8" />
+    <title>SimpleMemo | {$title}</title>
+    <link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="../public/css/main.css" />
+    <script defer src="../public/js/all.js"></script>
   </head>
   EOF;
 }
